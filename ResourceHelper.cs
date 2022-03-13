3fa299a65ef50769bdf819110939d781f859288c
Baka632 <stevemc123456@outlook.com>
2022-03-13 15:53:27 +0800

    大幅更改了Operator类

--- ResourceHelper.cs
+++ ResourceHelper.cs
@@ -17,110 +17,20 @@ namespace ArknightsResources.Operators.Resources
     public static class ResourceHelper
     {
         /// <summary>
-        /// 通过干员的图片代号获取其图片
+        /// 通过干员的立绘信息获取其图片
         /// </summary>
         /// <exception cref="ArgumentException"/>
         /// <exception cref="System.Resources.MissingManifestResourceException"/>
         /// <exception cref="System.Resources.MissingSatelliteAssemblyException"/>
-        /// <param name="codename">干员的图片代号</param>
-        /// <param name="type">干员的类型</param>
-        /// <param name="skinInfo">干员的皮肤信息,该参数在<see cref="OperatorType"/>为<see cref="OperatorType.Skin"/>时才使用,其余时候请传递null</param>
+        /// <param name="illustrationInfo">干员的立绘信息</param>
         /// <returns>一个byte数组,其中包含了干员的图片信息</returns>
-        public static byte[] GetOperatorImage(string codename, OperatorType type, OperatorSkinInfo? skinInfo)
+        public static byte[] GetOperatorImage(OperatorIllustrationInfo illustrationInfo)
         {
-            if (string.IsNullOrWhiteSpace(codename))
-            {
-                throw new ArgumentException($"“{nameof(codename)}”不能为 null 或空白。", nameof(codename));
-            }
-
-            string operatorType = "1";
-            switch (type)
-            {
-                case OperatorType.Elite0:
-                    operatorType = "1";
-                    break;
-                case OperatorType.Elite1:
-                    if (codename.Equals("amiya"))
-                    {
-                        return OperatorResources.operator_image_amiya_1_;
-                    }
-                    else
-                    {
-                        operatorType = "1";
-                    }
-                    break;
-                case OperatorType.Elite2:
-                    operatorType = "2";
-                    break;
-                case OperatorType.Skin:
-                    operatorType = skinInfo != null
-                        ? skinInfo.Value.ImageCodename
-                        : throw new ArgumentNullException(nameof(skinInfo), "当OperatorType为Skin时,必须传递参数OperatorSkinInfo");
-                    break;
-                case OperatorType.Promotion:
-                    operatorType = "promotion";
-                    break;
-                default:
-                    break;
-            }
-
-            string name = $"operator_image_{codename}_{operatorType}";
+            string name = $"operator_image_{illustrationInfo.ImageCodename}";
             byte[] value = (byte[])OperatorResources.ResourceManager.GetObject(name);
             if (value is null)
             {
-                throw new ArgumentException($@"使用给定的参数""{codename}"",""{operatorType}""和{(skinInfo.HasValue ? $@"""{skinInfo.Value}""" : "值为null的skinInfo")}""时找不到资源");
-            }
-            return value;
-        }
-
-        /// <summary>
-        /// 通过干员对象获取其图片
-        /// </summary>
-        /// <exception cref="ArgumentException"/>
-        /// <exception cref="System.Resources.MissingManifestResourceException"/>
-        /// <exception cref="System.Resources.MissingSatelliteAssemblyException"/>
-        /// <param name="op">干员对象</param>
-        /// <param name="type">干员的类型</param>
-        /// <param name="skinInfo">干员的皮肤信息,该参数在<see cref="OperatorType"/>为<see cref="OperatorType.Skin"/>时才使用,其余时候请传递null</param>
-        /// <returns>一个byte数组,其中包含了干员的图片信息</returns>
-        public static byte[] GetOperatorImage(Operator op, OperatorType type, OperatorSkinInfo? skinInfo)
-        {
-            string operatorType = "1";
-            switch (type)
-            {
-                case OperatorType.Elite0:
-                    operatorType = "1";
-                    break;
-                case OperatorType.Elite1:
-                    if (op.ImageCodename.Equals("amiya"))
-                    {
-                        return OperatorResources.operator_image_amiya_1_;
-                    }
-                    else
-                    {
-                        operatorType = "1";
-                    }
-                    break;
-                case OperatorType.Elite2:
-                    operatorType = "2";
-                    break;
-                case OperatorType.Skin:
-                    operatorType = skinInfo != null
-                        ? skinInfo.Value.ImageCodename
-                        : throw new ArgumentNullException(nameof(skinInfo), "当OperatorType为Skin时,必须传递参数OperatorSkinInfo");
-                    break;
-                case OperatorType.Promotion:
-                    operatorType = "promotion";
-                    break;
-                default:
-                    break;
-            }
-
-            string name = $"operator_image_{op.ImageCodename}_{operatorType}";
-            byte[] value = (byte[])OperatorResources.ResourceManager.GetObject(name);
-            if (value is null)
-            {
-                throw new ArgumentException($@"使用给定的参数""{op.Name}"",""{operatorType}""和{(skinInfo.HasValue ? $@"""{skinInfo.Value}""" : "值为null的skinInfo")}""时找不到资源");
+                throw new ArgumentException($@"使用给定的参数""{illustrationInfo}""时找不到资源");
             }
             return value;
         }
