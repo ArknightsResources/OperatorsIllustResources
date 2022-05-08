8edf877481caff8e9996870e66a0ee6cb8c41d63
Baka632 <stevemc123456@outlook.com>
2022-05-08 11:18:13 +0800

    可能的内存优化

--- InternalArrayPool.cs
+++ InternalArrayPool.cs
@@ -7,7 +7,7 @@ namespace ArknightsResources.Operators.Resources
 {
     internal static class InternalArrayPools
     {
-        internal static readonly ArrayPool<byte> ByteArrayPool = ArrayPool<byte>.Create(64 * 1024 * 1024, 3);
+        internal static readonly ArrayPool<byte> ByteArrayPool = ArrayPool<byte>.Create(1024 * 1024, 3);
         internal static readonly ArrayPool<byte[]> ByteArrayArrayPool = ArrayPool<byte[]>.Create(12, 3);
         internal static readonly ArrayPool<int> Int32ArrayPool = ArrayPool<int>.Create(64, 3);
     }
